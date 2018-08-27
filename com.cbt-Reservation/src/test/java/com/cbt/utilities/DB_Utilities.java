502dc15655f9f2b024e393239106e07dc79359dd
hasanistan <hasanistan1@gmail.com>
2018-08-27 00:19:11 -0500

    Updated DB utilities

--- com.cbt-Reservation/src/test/java/com/cbt/utilities/DB_Utilities.java
+++ com.cbt-Reservation/src/test/java/com/cbt/utilities/DB_Utilities.java
@@ -14,6 +14,11 @@ private static Connection myConn;
 private static Statement statement;
 private static ResultSet resultSet;
 
+	public static void main(String[] args) {
+		boolean b = establishConnection();
+		System.out.println(b);
+	}
+	
 	
 	public static boolean establishConnection() {
 		String url = ConfigReader.getProperties("connectionString");
@@ -26,29 +31,86 @@ private static ResultSet resultSet;
 			e.printStackTrace();
 		}
 		
-		if(myConn!= null)			return true ; 
+		if(myConn!= null)return true ; 
 		return false;
 	}
 	
-	public List<Map<String, Object>> getQueryResult(String query) throws SQLException{
-		
+	
+	public static List<Map<String, Object>> getQueryResultMap(String query) {
+		executeQuery(query);
 		List<Map<String, Object>> queryResult = new ArrayList<>();
-		
-		statement = myConn.createStatement();
-		resultSet = statement.executeQuery(query);
-		
-		ResultSetMetaData rsmd = resultSet.getMetaData();
-		
-		while(resultSet.next()) {
-			
-			Map<String, Object> resultMap = new HashMap<>();
-			
-			for(int i = 1; i < rsmd.getColumnCount(); i++) {
-				resultMap.put(rsmd.getColumnName(i), resultSet.getObject(i));
+		ResultSetMetaData rsmd;
+
+		try {
+			rsmd = resultSet.getMetaData();
+			while (resultSet.next()) {
+				Map<String, Object> resultMap = new HashMap<>();
+				for (int i = 1; i <= rsmd.getColumnCount(); i++) {
+					resultMap.put(rsmd.getColumnName(i), resultSet.getObject(i));
+				}
+				queryResult.add(resultMap);
 			}
-			queryResult.add(resultMap);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		
 		return queryResult;
 	}
+	
+	public static List<String> getColumnNames(String query) {
+		executeQuery(query);
+		List<String> columns = new ArrayList<>();
+		ResultSetMetaData rsmd;
+
+		try {
+			rsmd = resultSet.getMetaData();
+			int columnCount = rsmd.getColumnCount();
+
+			for (int i = 1; i <= columnCount; i++) {
+				columns.add(rsmd.getColumnName(i));
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return columns;
+
+	}
+	
+	private static void executeQuery(String query) {
+		try {
+			statement = myConn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			resultSet = statement.executeQuery(query);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public static void destroy() {
+		try {
+			if (resultSet != null) {
+				resultSet.close();
+			}
+			if (statement != null) {
+				statement.close();
+			}
+			if (connection != null) {
+				connection.close();
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
 }
